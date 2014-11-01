8b46ac5a5f847c0a623eec9a019bc917be5dcf66
Srilakshmi <sc3772@columbia.edu>
2014-11-01 00:56:01 -0400

    Please enter the commit message for your changes. Lines starting

--- cronjob.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-java -cp  /home/ec2-user/madhuri_sri/TweetSave.jar com.example.helloworld.core.MainClass | tee /var/www/tweetSave.hourly.log &
-sleep 300
-echo 'closing Tweet'
-ps aux | grep TweetSave.jar
-
-pkill -f 'TweetSave'
-echo 'closed Tweet Saving Job'
-ps aux | grep TweetSave.jar
